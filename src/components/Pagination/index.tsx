1c1a5bb8ac1fbaeb94956ed3afcc2002325935a7
jonathanwlopes <jonathan@agenciaeplus.com.br>
2022-02-14 08:38:52 -0300

    initial pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,30 @@
 import { Box, HStack } from "@chakra-ui/react"
 import { PaginationItem } from "./PaginationItem"
 
-export const Pagination = () => {
+interface PaginationProps {
+  totalCountOfRegisters: number
+  registerPerPage?: number
+  currentPage?: number
+  onPageChange: (page: number) => void
+}
+
+const siblingsCount = 1
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1
+    })
+    .filter((page) => page > 0)
+}
+
+export const Pagination = ({ totalCountOfRegisters, registerPerPage = 10, currentPage = 1, onPageChange }: PaginationProps) => {
+  const lastPage = Math.floor(totalCountOfRegisters / registerPerPage)
+
+  const previousPage = currentPage > 1 ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1) : []
+
+  const nextPage = currentPage < lastPage ? generatePagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage)) : []
+
   return (
     <HStack mt="8" justify="space-between" align="center" spacing="6">
       <Box>
@@ -9,12 +32,9 @@ export const Pagination = () => {
       </Box>
 
       <HStack spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
-        <PaginationItem number={5} />
-        <PaginationItem number={6} />
+        {previousPage.length > 0 && previousPage.map((page) => <PaginationItem key={page} number={page} />)}
+        <PaginationItem number={currentPage} isCurrent />
+        {nextPage.length > 0 && nextPage.map((page) => <PaginationItem key={page} number={page} />)}
       </HStack>
     </HStack>
   )
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,29 +1,34 @@
 import { useQuery } from "react-query"
 import { api } from "../api"
 
-export const useUsers = () => {
-  return useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("/users")
+type User = {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+}
 
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        }
-      })
+export const getUsers = async (): Promise<User[]> => {
+  const { data } = await api.get("/users")
 
-      return users
-    },
-    {
-      staleTime: 1000 * 5, // 5 seconds
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
     }
-  )
+  })
+
+  return users
+}
+
+export const useUsers = () => {
+  return useQuery("users", getUsers, {
+    staleTime: 1000 * 5, // 5 seconds
+  })
 }
